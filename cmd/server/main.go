d164fe4d9817fc95c7d0f1947aed2bd83c1c6a47
wangzheng <wangzheng@powerlaw.ai>
2024-06-07 22:16:41 +0800

    123

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,24 +2,36 @@ package main
 
 import (
 	"context"
+	"errors"
+	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"time"
-	api "wangzheng/brain/cmd/server/api"
-
-	"github.com/gin-gonic/gin"
 )
 
-const DefaultPort = ":8080"
-
 func main() {
-	router := gin.Default()
-	api.InitApis(router)
+	r := gin.Default()
+	gin.DebugPrintRouteFunc = func(httpMethod, absolutePath, handlerName string, nuHandlers int) {
+		log.Printf("endpoint %v %v %v %v\n", httpMethod, absolutePath, handlerName, nuHandlers)
+	}
+
+	r.POST("/foo", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "foo")
+	})
+
+	r.GET("/bar", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "bar")
+	})
+
+	r.GET("/status", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "ok")
+	})
 
 	srv := &http.Server{
-		Addr: ":8000",
+		Handler: r,
+		Addr:    ":8000",
 		// ReadHeaderTimeout is the amount of time allowed to read
 		// request headers. The connection's read deadline is reset
 		// after reading the headers and the Handler can decide what
@@ -27,7 +39,6 @@ func main() {
 		// is zero, the value of ReadTimeout is used. If both are
 		// zero, there is no timeout.
 		ReadHeaderTimeout: 15 * time.Second,
-
 		// ReadTimeout is the maximum duration for reading the entire
 		// request, including the body. A zero or negative value means
 		// there will be no timeout.
@@ -37,14 +48,12 @@ func main() {
 		// upload rate, most users will prefer to use
 		// ReadHeaderTimeout. It is valid to use them both.
 		ReadTimeout: 15 * time.Second,
-
 		// WriteTimeout is the maximum duration before timing out
 		// writes of the response. It is reset whenever a new
 		// request's header is read. Like ReadTimeout, it does not
 		// let Handlers make decisions on a per-request basis.
 		// A zero or negative value means there will be no timeout.
 		WriteTimeout: 10 * time.Second,
-
 		// IdleTimeout is the maximum amount of time to wait for the
 		// next request when keep-alives are enabled. If IdleTimeout
 		// is zero, the value of ReadTimeout is used. If both are
@@ -54,24 +63,21 @@ func main() {
 
 	go func() {
 		// 服务连接
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
 			log.Fatalf("listen: %s\n", err)
 		}
 	}()
 
-	// Wait for an interrupt signal to gracefully shutdown the server (with a 5-second timeout)
-	quit := make(chan os.Signal, 1) // Adding buffer size 1 to the channel
+	// 等待中断信号以优雅地关闭服务器（设置 5 秒的超时时间）
+	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt)
 	<-quit
-
-	log.Println("Shutting down the server...")
+	log.Println("Shutdown Server ...")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-
 	if err := srv.Shutdown(ctx); err != nil {
 		log.Fatal("Server Shutdown:", err)
 	}
-
-	log.Println("Server has shut down")
+	log.Println("Server exiting")
 }
